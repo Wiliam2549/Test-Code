9e54068b1929aa63954ded02718686882dd24c74
Wiliam2549 <99852371+Wiliam2549@users.noreply.github.com>
2022-02-16 20:01:18 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,37 @@
+var test = 0
+var a
+
+function setup() {
+  createCanvas(400,400);
+  test = 5;
+  sprite = createSprite(200, 200, 10, 10);
+  sprite.shapeColor = "blue";
+  a = createEdgeSprites();
+}
+
+function draw() {
+  background("red");
+  console.log(test);
+  //first edge in array a is the left edge
+  //second edge is the right edge
+
+  sprite.bounceOff(a[2]);
+  if(keyDown(LEFT_ARROW)) {
+    sprite.velocityX = -5
+  }
+  if(keyDown(RIGHT_ARROW)) {
+      sprite.velocityX = 5
+  }
+  if(keyDown(DOWN_ARROW)) {
+    sprite.velocityY = 5
+  }
+  if(keyDown(UP_ARROW)) {
+    sprite.velocityY = -5
+  }
+
+  drawSprites();
+}
+
+
+
+
